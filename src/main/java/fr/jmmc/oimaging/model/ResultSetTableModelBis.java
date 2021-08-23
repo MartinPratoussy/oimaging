e28ae794d64ba34445a40a6a1b2dd40d28026c02
Martin Pratoussy <martin.pratoussy@cpe.fr>
2021-08-23 13:53:36 +0200

    Merge fits headers cards

--- src/main/java/fr/jmmc/oimaging/model/ResultSetTableModelBis.java
+++ src/main/java/fr/jmmc/oimaging/model/ResultSetTableModelBis.java
@@ -1,47 +1,57 @@
+/*******************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ ******************************************************************************/
 package fr.jmmc.oimaging.model;
 
 import fr.jmmc.oimaging.services.ServiceResult;
 import fr.jmmc.oitools.fits.FitsHeaderCard;
-import fr.jmmc.oitools.image.ImageOiData;
 import fr.nom.tam.fits.FitsException;
-import org.apache.commons.fileupload.util.Streams;
-
-import javax.swing.table.AbstractTableModel;
 import javax.swing.table.DefaultTableModel;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
+/**
+ *
+ * @author martin
+ */
 public class ResultSetTableModelBis extends DefaultTableModel {
 
     private final List<List<FitsHeaderCard>> fitsHeadersCards = new ArrayList<List<FitsHeaderCard>>();
-    private List<ServiceResult> results = new ArrayList<>();
-    private String[] headers;
+    private final List<ServiceResult> results = new ArrayList<>();
+    private List<String> headers = new ArrayList<>();
 
     public ResultSetTableModelBis() {
         super();
     }
 
+    /**
+     * Redesign entirely the result table with the new headers and the new values
+     * @param results The result set
+     */
     public void populate(List<ServiceResult> results) {
-        this.results = results;
+        fitsHeadersCards.clear();
+        this.results.clear();
+        this.results.addAll(results);
         this.results.forEach(result -> {
             try {
-                fitsHeadersCards.clear();
                 fitsHeadersCards.add(result.getOifitsFile().getImageOiData().getOutputParam().getHeaderCards());
             } catch (IOException | FitsException e) {
                 e.printStackTrace();
             }
         });
 
-        System.out.println(Stream.of(fitsHeadersCards).flatMap(Collection::stream).collect(Collectors.toList()));
+        // Merge the results parameters into one list with all the parameters
+        headers = fitsHeadersCards
+                .stream()
+                .flatMap(List::stream)
+                .map(FitsHeaderCard::getKey)
+                .distinct()
+                .collect(Collectors.toList());
 
-        // TODO: add cards keys in headers
-
-        setColumnCount((headers != null) ? headers.length : 0);
-        setColumnIdentifiers(headers);
+        setColumnCount(headers.size());
+        setColumnIdentifiers(headers.toArray());
 
         fireTableStructureChanged();
     }
@@ -53,7 +63,7 @@ public class ResultSetTableModelBis extends DefaultTableModel {
 
     @Override
     public int getColumnCount() {
-        return (headers != null) ? headers.length : 0;
+        return headers.size();
     }
 
     @Override
